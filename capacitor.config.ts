6acfd8899aa6b5bfc72acb6dedbc749879666695
Cristofer Leonardo Salazar Palacios <crisxxsalazar15@gmail.com>
2024-05-22 08:54:12 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'helloIonic',
+  webDir: 'www'
+};
+
+export default config;
